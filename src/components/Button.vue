36c834392cdce3ead392b72075e23ef890029c60
Lukas Matousek <matousek1199@gmail.com>
2023-06-19 21:24:54 +0200

    button added

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  text: string
+  color: string
+}>()
+</script>
+
+<template>
+  <p class="button">{{ text }}</p>
+</template>
+
+<style scoped>
+.button {
+  color: white;
+  background-color: v-bind(color);
+  margin: 2px 4px;
+  padding: 3px 8px;
+  border-radius: 4px;
+  cursor: pointer;
+}
+</style>
--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,15 +1,27 @@
+<script setup lang="ts">
+import CustomButton from './Button.vue'
+</script>
+
 <template>
   <div class="container">
     <p class="text">TodoItem</p>
+    <div class="button-container">
+      <CustomButton text="Done" color="green" />
+      <CustomButton text="Delete" color="red" />
+    </div>
   </div>
 </template>
 
 <style scoped>
 .container {
   display: flex;
+  align-items: center;
   background-color: rgb(253, 253, 253);
   border: 1px solid lightgrey;
   border-radius: 5px;
   padding: 4px 8px;
+}
+.button-container {
+  display: flex;
 }
 </style>
