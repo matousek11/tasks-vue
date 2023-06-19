cfb6ef784a9ff170610a14958cd85426382cdbb1
Lukas Matousek <matousek1199@gmail.com>
2023-06-19 22:16:20 +0200

    Design of button

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 import ToDoItem from './components/ToDoItem.vue'
+import { ref } from 'vue'
+
+const array = ref([
+  { id: '1', text: 'string', done: false },
+  { id: '2', text: 'string2', done: true }
+])
 </script>
 
 <template>
-  <ToDoItem todoItem="text" />
-  <ToDoItem todoItem="text" />
+  <div>
+    <ToDoItem :id="item.id" :text="item.text" :done="item.done" v-for="item in array" />
+  </div>
 </template>
 
 <style scoped></style>
--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -2,7 +2,9 @@
 import CustomButton from './Button.vue'
 
 defineProps<{
-  todoItem: string
+  id: string
+  text: string
+  done: boolean
 }>()
 
 const handleDone = () => {
@@ -16,26 +18,51 @@ const handleDelete = () => {
 
 <template>
   <div class="container">
-    <p class="text">{{ todoItem }}</p>
+    <div class="dark-container" v-bind:style="done === true ? '' : 'display: none;'"></div>
+    <p class="text">{{ text }}</p>
     <div class="button-container">
       <CustomButton text="Done" color="green" @on-press-function="handleDone" />
       <CustomButton text="Delete" color="red" @on-press-function="handleDelete" />
     </div>
+    <hr class="line" v-bind:style="done == true ? '' : 'display: none;'" />
   </div>
 </template>
 
 <style scoped>
 .container {
-  min-width: 250px;
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: space-between;
+  min-width: 250px;
   background-color: rgb(253, 253, 253);
+  margin-bottom: 5px;
+  padding: 4px 8px;
   border: 1px solid lightgrey;
   border-radius: 5px;
-  padding: 4px 8px;
+}
+.dark-container {
+  position: absolute;
+  left: -1px;
+  width: 100%;
+  height: 100%;
+  min-width: 250px;
+  background-color: rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+  z-index: 10;
+  pointer-events: none;
 }
 .button-container {
   display: flex;
+}
+.line {
+  position: absolute;
+  left: -10%;
+  width: 120%;
+  height: 1px;
+  color: transparent;
+  background-color: black;
+  border-color: transparent;
+  pointer-events: none;
 }
 </style>
